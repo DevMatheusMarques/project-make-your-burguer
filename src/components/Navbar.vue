4a68eb1fb9d63517f8115133e2e164787da91602
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-07-01 20:27:46 -0300

    Vue Router Implementation

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<template>
+  <nav id="nav">
+    <router-link to="/">
+      <img src="/img/logo.png" alt="Logotipo da Empresa" id="logo">
+    </router-link>
+    <router-link to="/">
+      Home
+    </router-link>
+    <router-link to="/pedidos">
+      Pedidos
+    </router-link>
+  </nav>
+</template>
+
+<style scoped>
+
+</style>
