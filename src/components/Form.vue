db7f80721950c6c1f1e0116d010892476ff844be
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-07-02 12:36:56 -0300

    Creating the system message component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,43 +1,10 @@
-<script>
-export default {
-  name: "Form",
-  data() {
-    return {
-      breads: null,
-      meats: null,
-      optionaldata: null,
-      name: null,
-      bread: null,
-      meat: null,
-      optional: [],
-      status: "Solicitado",
-      msg: null
-    }
-  },
-  methods: {
-    async getIngredients() {
-      const req = await fetch("http://localhost:3000/ingredientes");
-      const data = await req.json();
-
-      this.breads = data.paes;
-      this.meats = data.carnes;
-      this.optionaldata = data.opcionais;
-
-    }
-  },
-  mounted() {
-    this.getIngredients();
-  }
-}
-</script>
-
 <template>
   <div>
-    <p>Componente de Mensagem</p>
-    <div class="form">
+    <Message :msg="msg" v-show="msg"/>
+    <form class="form" method="POST" @submit.prevent="createBurger">
       <div class="form__input">
         <label for="name">Nome do cliente:</label>
-        <input type="text" name="name" id="name" v-model="nome" placeholder="Digite o nome do cliente" required>
+        <input type="text" name="name" id="name" v-model="name" placeholder="Digite o nome do cliente" required>
       </div>
       <div class="form__input">
         <label for="bread">Escolha o tipo de pão:</label>
@@ -50,7 +17,7 @@ export default {
       </div>
       <div class="form__input">
         <label for="meat">Escolha o tipo de carne:</label>
-        <select name="meat" id="bread" v-model="meat" required>
+        <select name="meat" id="meat" v-model="meat" required>
           <option value=""></option>
           <option v-for="meat in meats" :key="meat.id" :value="meat.tipo">{{meat.tipo}}</option>
         </select>
@@ -65,80 +32,158 @@ export default {
       <div class="form__input">
         <input type="submit" value="Finalizar Pedido" class="form__input-submit">
       </div>
-    </div>
+    </form>
   </div>
 </template>
 
+<script>
+import Message from "@/components/Message.vue";
+
+export default {
+  name: "Form",
+  components: {Message},
+  data() {
+    return {
+      breads: null,
+      meats: null,
+      optionaldata: null,
+      name: null,
+      bread: null,
+      meat: null,
+      optionals: [],
+      status: "Solicitado",
+      msg: null
+    }
+  },
+  methods: {
+    async getIngredients() {
+      try {
+        const req = await fetch("http://localhost:3000/ingredientes");
+        const data = await req.json();
+
+        this.breads = data.paes;
+        this.meats = data.carnes;
+        this.optionaldata = data.opcionais;
+      } catch (error) {
+        console.error('Erro ao buscar os ingredientes:', error);
+      }
+    },
+
+    async createBurger(e) {
+      e.preventDefault();
+
+      const data = {
+        name: this.name,
+        bread: this.bread,
+        meat: this.meat,
+        optional: Array.from(this.optionals),
+        status: "Solicitado"
+      }
+
+      const dataJson = JSON.stringify(data);
+
+      try {
+        const req = await fetch("http://localhost:3000/burgers", {
+          method: "POST",
+          headers: { "Content-Type" : "application/json" },
+          body: dataJson
+        });
+
+        const res = await req.json();
+
+        console.log(res);
+
+        this.msg = `Pedido realizado com sucesso!`;
+
+        // Limpar mensagem após 3 segundos
+        setTimeout(() => this.msg = "", 3000);
+
+        // Limpar campos
+        this.name = "";
+        this.meat = "";
+        this.bread = "";
+        this.optionals = [];
+      } catch (error) {
+        console.error('Erro ao criar o pedido:', error);
+      }
+    }
+  },
+  mounted() {
+    this.getIngredients();
+  }
+}
+</script>
+
 <style scoped>
-  .form {
-    max-width: 400px;
-    margin: 0 auto;
-  }
+.form {
+  max-width: 400px;
+  margin: 0 auto;
+}
 
-  .form__input {
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
-  }
+.form__input {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+}
 
-  label {
-    font-weight: bold;
-    margin-bottom: 15px;
-    color: #222;
-    padding: 5px 10px;
-    border-left: 4px solid #FCBA03;
-  }
+label {
+  font-weight: bold;
+  margin-bottom: 15px;
+  color: #222;
+  padding: 5px 10px;
+  border-left: 4px solid #FCBA03;
+}
 
-  input, select {
-    padding: 5px 10px;
-    width: 300px;
-    border: 2px solid #222;
-    border-radius: 6px;
-  }
+input, select {
+  padding: 5px 10px;
+  width: 300px;
+  border: 2px solid #222;
+  border-radius: 6px;
+}
 
-  .form__input-optional {
-    flex-direction: row;
-    flex-wrap: wrap;
-  }
+.form__input-optional {
+  flex-direction: row;
+  flex-wrap: wrap;
+}
 
-  .form__input-title {
-    width: 100%;
-  }
+.form__input-title {
+  width: 100%;
+}
 
-  .form__input-checkbox {
-    display: flex;
-    align-items: flex-start;
-    width: 50%;
-    margin-bottom: 20px;
-  }
+.form__input-checkbox {
+  display: flex;
+  align-items: flex-start;
+  width: 50%;
+  margin-bottom: 20px;
+}
 
-  .form__input-checkbox span, .form__input-checkbox input {
-    width: auto;
-  }
+.form__input-checkbox span, .form__input-checkbox input {
+  width: auto;
+}
 
-  .form__input-checkbox span {
-    margin-left: 6px;
-    font-weight: bold;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+.form__input-checkbox span {
+  margin-left: 6px;
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
 
-  .form__input-submit {
-    background-color: #FCBA03;
-    color: #222;
-    font-weight: bold;
-    border: 2px solid #222;
-    border-radius: 6px;
-    padding: 10px;
-    font-size: 16px;
-    margin: 0 auto;
-    cursor: pointer;
-    transition: .5s;
-  }
+.form__input-submit {
+  background-color: #FCBA03;
+  color: #222;
+  font-weight: bold;
+  border: 2px solid #222;
+  border-radius: 6px;
+  padding: 10px;
+  font-size: 16px;
+  margin: 0 auto;
+  cursor: pointer;
+  transition: .5s;
+}
 
-  .form__input-submit:hover {
-    background-color: #f8cf5e;
-    color: #222;
-  }
+.form__input-submit:hover {
+  background-color: #f8cf5e;
+  color: #222;
+}
 </style>
---
+++ src/components/Message.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  name: "Message",
+  props: {msg: String}
+}
+</script>
+
+<template>
+  <div class="container__message">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<style scoped>
+.container__message {
+  color: #004085;
+  background-color: #CCE5FF;
+  border: 2px solid #B8DAFF;
+  border-radius: 6px;
+  padding: 10px;
+  max-width: 400px;
+  margin: 30px auto;
+}
+</style>
